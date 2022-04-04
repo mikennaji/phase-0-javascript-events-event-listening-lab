df3e275dd65c237d02cc678b107d86e7313aa977
mikennaji <mnnaji3@uwo.ca>
2022-04-04 14:09:08 -0400

    done

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const input = document.getElementById('input');
+
 function addingEventListener() {
+  
+    input.addEventListener('click',clickAlert)
+
 }
+    
+
+function clickAlert(){
+        alert('I was clicked!');
+
+    };
+
+
